b89804df7006b1afe073ef1ad7ecc8540efe3d1d
Jorge6092 <jorgeofrancisc@gmail.com>
2023-06-28 11:31:04 -0600

    Objetos

---
+++ Objeto.js
@@ -0,0 +1,11 @@
+let Cuenta_Bancaria = {
+    tipo_de_cuenta : "estandar",
+    titular: "Jorge Francisco",
+    saldo: "$0.05",
+    metodos:{
+        depositar: "agragar dinero",
+        extraer: "sacar dinero"
+    }
+};
+console.log(Cuenta_Bancaria[tipo_de_cuenta]);
+//no deja
